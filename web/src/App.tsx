72d933d3ce3be3f1c7eeaa1c5ba3adae5979e542
marcelosperalta <marcelosperalta@gmail.com>
2020-10-14 19:14:06 +0200

    rocketseat

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoImg from './images/logo.svg';
 
 // react component
 // https://reactjs.org/docs/components-and-props.html
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"></img>
 
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Berlim</strong>
-          <span>Alemanha</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba{0, 0, 0, 0.6}" />
-        </a>
-      </div>
-    </div>
   );
 }
 
