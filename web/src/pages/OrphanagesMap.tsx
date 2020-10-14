7e98cd507af5885ba3d643c42e1d86c4316f60e8
marcelosperalta <marcelosperalta@gmail.com>
2020-10-14 20:22:47 +0200

    rocketseat

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
 
 import mapMarketImg from '../images/map-marker.svg';
 
@@ -7,10 +9,23 @@ function OrphanagesMap() {
         <div id="page-map">
             <aside>
                 <header>
-                    <img src="" alt=""/>
+                    <img src={mapMarketImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
 
+                <footer>
+                    <strong>Berlim</strong>
+                    <span>Alemanha</span>
+                </footer>
             </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
         </div>
     )
 }
