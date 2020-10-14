986370b1d5b1ad7fc493dec4f4781d6d3c6e1fa9
marcelosperalta <marcelosperalta@gmail.com>
2020-10-14 16:20:44 +0200

    rocketseat

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
 import './styles/global.css'
+import './styles/pages/landing.css'
 
+import logoImg from './images/logo.svg'
+
+// react component
+// https://reactjs.org/docs/components-and-props.html
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"></img>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Berlim</strong>
+          <span>Alemanha</span>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
 
 /*
 // typescript interface 
@@ -32,14 +61,3 @@ function App() {
 }
 */
 
-// react component
-// https://reactjs.org/docs/components-and-props.html
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-}
-
-export default App;
