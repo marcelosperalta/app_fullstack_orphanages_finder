984e1e67bb511296ad80beebbe073ccca792a659
marcelosperalta <marcelosperalta@gmail.com>
2020-10-14 16:10:57 +0200

    rocketseat

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 
+import './styles/global.css'
+
+
+/*
+// typescript interface 
+// https://www.typescriptlang.org/docs/handbook/interfaces.html
+interface TitleProps {
+  text: string;
+}
+
+// react component
+// https://reactjs.org/docs/components-and-props.html
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+// react component
+// https://reactjs.org/docs/components-and-props.html
 function App() {
   return (
     <div className="App">
-      <h1>Hello NLW</h1>
+      <Title text="Título 1" />
+      <Title text="Título 2" />
+      <Title text="Título 3" />
+      <Title text="Título 4" />
+    </div>
+  );
+}
+*/
+
+// react component
+// https://reactjs.org/docs/components-and-props.html
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
     </div>
   );
 }
