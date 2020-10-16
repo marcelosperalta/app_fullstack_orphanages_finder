c0e3eb38f99d483f403fb58b2b8f4c1233102de5
marcelosperalta <marcelosperalta@gmail.com>
2020-10-16 12:43:56 +0200

    rocketseat

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
 // backend > request and response
 //               REQ  /  RES
 
-// ----- HTTP methods = GET, POST, PUT, DELETE -----
+// ----- HTTP methods = GET, POST, PUT, DELETE -------
 // GET    > (search) requests a representation of the specified resource
 // POST   > (create) submit an entity to the specified resource
 // PUT    > (update) replaces all current representations of the target resource
 // DELETE > (delete) deletes the specified resource
-
+// * when you try to access some website or website resource using a browser, you are using the GET method
+// e.g.
+// https://rocketseat.com.br/
+// https://rocketseat.com.br/copyright
+// * when you need to test the POST, PUT, DELETE methods, you can use an API tester
+// e.g.
+// https://insomnia.rest/
+// https://www.postman.com/
+// ---------------------------------------------------
 
 import express from 'express';
 
