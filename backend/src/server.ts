6abedcc7bd66c1122718dbc8495e0e634db9bbf9
marcelosperalta <marcelosperalta@gmail.com>
2020-10-17 15:53:40 +0200

    rocketseat

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,44 +17,48 @@
 // ---------------------------------------------------
 
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 // to turn Express able to understand JSON
 app.use(express.json());
+app.use(routes);
 
-app.post('/orphanages', async (request, response) => {
-    // console.log(request.body);
-    const {
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends
-          } = request.body;
 
-    const orphanagesRepository = getRepository(Orphanage);
+// ----------------- moved to "routes.ts" -----------------
+// app.post('/orphanages', async (request, response) => {
+//     // console.log(request.body);
+//     const {
+//             name,
+//             latitude,
+//             longitude,
+//             about,
+//             instructions,
+//             opening_hours,
+//             open_on_weekends
+//           } = request.body;
 
-    const orphanage = orphanagesRepository.create({
-            name,
-            latitude,
-            longitude,
-            about,
-            instructions,
-            opening_hours,
-            open_on_weekends
-    });
+//     const orphanagesRepository = getRepository(Orphanage);
 
-    await orphanagesRepository.save(orphanage);
+//     const orphanage = orphanagesRepository.create({
+//             name,
+//             latitude,
+//             longitude,
+//             about,
+//             instructions,
+//             opening_hours,
+//             open_on_weekends
+//     });
 
-    return response.status(201).json(orphanage);
-});
+//     await orphanagesRepository.save(orphanage);
+
+//     return response.status(201).json(orphanage);
+// });
+// ----------------- moved to "routes.ts" -----------------
+
 // resource = users ----------------------------------
 // HTTP method = GET ---------------------------------
 // <route> -------------------------------------------
