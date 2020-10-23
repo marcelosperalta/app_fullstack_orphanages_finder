fa1eb604f248e92f6c433c088c82e0ebaf563498
marcelosperalta <marcelosperalta@gmail.com>
2020-10-23 20:59:05 +0200

    rocketseat

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import Leaflet, { icon } from 'leaflet';
 
 import 'leaflet/dist/leaflet.css'
 
 import mapMarketImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarketImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68]
+})
 
 function OrphanagesMap() {
     return (
@@ -43,6 +50,11 @@ function OrphanagesMap() {
                 // light:
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 // styles: https://docs.mapbox.com/mapbox-gl-js/api/map/
+                />
+
+                <Marker 
+                    icon={mapIcon}
+                    position={[52.5067614,13.2846514]}
                 />
             </Map>
 
