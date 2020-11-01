6738870db69bfd0e0a5ec47dfa282e7e0cb871a5
marcelosperalta <marcelosperalta@gmail.com>
2020-11-01 19:59:53 +0100

    rocketseat

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
@@ -9,6 +9,7 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarketImg from '../images/map-marker.svg';
 import mapIcon from '../utils/mapIcon';
+import api from "../services/api";
 
 import '../styles/pages/orphanages-map.css';
 // import App from '../App';
@@ -22,6 +23,12 @@ import '../styles/pages/orphanages-map.css';
 // })
 
 function OrphanagesMap() {
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            console.log(response);
+        });
+    }, []);
+
     return (
         <div id="page-map">
             <aside>
