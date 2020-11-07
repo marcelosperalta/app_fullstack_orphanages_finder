1b2f0ad1be9497f4f99efcbd74eac66d20187d96
marcelosperalta <marcelosperalta@gmail.com>
2020-11-07 11:04:59 +0100

    rocketseat

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -26,11 +26,12 @@ interface Orphanage {
   latitude: number;
   longitude: number;
   name: string;
-  description: string;
+  about: string;
   instructions: string;
   opening_hours: string;
   open_on_weekends: string;
   images: Array<{
+    id: number;
     url: string;
   }>;
 }
@@ -75,7 +76,7 @@ export default function Orphanage() {
           <img src={orphanage.images[0].url} alt={orphanage.name} />
 
           <div className="images">
-            <button className="active" type="button">
+            {/* <button className="active" type="button">
               <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
             </button>
             <button type="button">
@@ -92,7 +93,15 @@ export default function Orphanage() {
             </button>
             <button type="button">
               <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
+            </button> */}
+            {orphanage.images.map(image => {
+              return (
+                <button key={image.id} className="active" type="button">
+                {/* <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" /> */}
+                <img src={image.url} alt={orphanage.name} />
+              </button>
+              );
+            })}
           </div>
           
           <div className="orphanage-details-content">
@@ -100,7 +109,7 @@ export default function Orphanage() {
             <h1>{orphanage.name}</h1>
             {/* <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p> */}
             <p>
-              {orphanage.description}
+              {orphanage.about}
             </p>
 
             <div className="map-container">
@@ -122,32 +131,44 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                {/* <a href="">Ver rotas no Google Maps</a> */}
+                <a target="_blank"  rel="noopener noreferrer" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
             <hr />
 
             <h2>Instruções para visita</h2>
-            <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+            {/* <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p> */}
+            <p>{orphanage.instructions}</p>
 
             <div className="open-details">
               <div className="hour">
                 <FiClock size={32} color="#15B6D6" />
                 Segunda à Sexta <br />
-                8h às 18h
+                {/* 8h às 18h */}
+                {orphanage.opening_hours}
               </div>
-              <div className="open-on-weekends">
-                <FiInfo size={32} color="#39CC83" />
-                Atendemos <br />
-                fim de semana
-              </div>
+
+              { orphanage.open_on_weekends ? (
+                <div className="open-on-weekends">
+                  <FiInfo size={32} color="#39CC83" />
+                  Atendemos <br />
+                  fim de semana
+                </div>
+              ) : (
+                <div className="open-on-weekends dont-open">
+                  <FiInfo size={32} color="#FF669D" />
+                  Não atendemos <br />
+                  fim de semana
+                </div>
+              ) }
             </div>
 
-            <button type="button" className="contact-button">
+            {/* <button type="button" className="contact-button">
               <FaWhatsapp size={20} color="#FFF" />
               Entrar em contato
-            </button>
+            </button> */}
           </div>
         </div>
       </main>
